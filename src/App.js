2e8cc3072956558e467fdf3a96ee6dd4d0dbb6a0
MD REJAUR RAHMAN <you@example.com>
2022-07-29 19:32:56 +0600

    para added

--- src/App.js
+++ src/App.js
@@ -4,6 +4,11 @@ function App() {
   return (
     <div className="App">
       <h1>Welcome to Onito test task</h1>
+      <p>
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate illo
+        numquam minima magnam ad sit. Atque mollitia vero maxime inventore,
+        cumque ex praesentium qui vitae rem. Quaerat consequatur sunt sequi!
+      </p>
     </div>
   );
 }
